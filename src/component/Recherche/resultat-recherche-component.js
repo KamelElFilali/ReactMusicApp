33604d9dab3c4d3de3d5485898ec5b89a3165986
Christian Djipsu <cdjipsum@isi-mtl.com>
2020-09-17 18:27:24 -0400

    resultat recherce

--- src/component/Recherche/resultat-recherche-component.js
+++ src/component/Recherche/resultat-recherche-component.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ResultatRechercheComponent = ({ textP, url, masterId, onClick }) => (
+const ResultatRechercheComponent = ({ textPs, url, masterId, onClick }) => (
     <div className='container'>
         <img class='card-img-top' src={url} alt='Card image cap' />
         <div class='card-body'>
-            <p class='card-text'>{textP}</p>
+            <p class='card-text'>{textPs}</p>
             <button onClick={onClick} data-key={masterId} type='button' class='btn btn-primary'>Details</button>
         </div>
     </div>
