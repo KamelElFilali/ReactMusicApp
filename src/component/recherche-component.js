4982d0cbfbf92334fb5449f9c88c002260ae0066
Christian Djipsu <cdjipsum@isi-mtl.com>
2020-09-16 21:07:00 -0400

    maj

--- src/component/recherche-component.js
+++ src/component/recherche-component.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const RechercheComponent = () => (
-    <div className='' />
+    <div className=''>
+
+        <h1>RECHERCHE</h1>
+
+    </div>
+
 )
 
 export default RechercheComponent
