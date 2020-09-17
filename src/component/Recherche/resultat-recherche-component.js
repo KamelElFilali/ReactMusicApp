89cce210dcee666c05353dc44999ab67802b6661
Christian Djipsu <cdjipsum@isi-mtl.com>
2020-09-17 10:55:50 -0400

    test

--- src/component/Recherche/resultat-recherche-component.js
+++ src/component/Recherche/resultat-recherche-component.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const ResultatRechercheComponent = () => (
-    <div className='' />
+const ResultatRechercheComponent = ({ onClick, textH5, textP, url }) => (
+    <div className='container'>
+        <div>
+            <img class='card-img-top' src={url} alt='Card image cap' />
+            <div class='card-body'>
+                <h5 class='card-title'>{textH5}</h5>
+                <p class='card-text'>{textP}</p>
+                <button onClick={onClick} data-key='master_id' type='button' class='btn btn-primary'>Details</button>
+            </div>
+        </div>
+    </div>
 )
 
 export default ResultatRechercheComponent
--- src/container/Recherche/resultat-recherche-container.js
+++ src/container/Recherche/resultat-recherche-container.js
@@ -9,12 +9,26 @@ class ResultatRechercheContainer extends Component {
         this.state = {
 
         }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    componentDidMount () {
+        fetch('http://localhost:8080/recherches', { method: 'GET' })
+            .then(response => response.json())
+            .then(responseJson => {
+                responseJson.master_id
+            })
+    }
+
+    handleClick (event) {
+        console.log(event.currentTarget.getAttribute('data-key'), ' - ', event.currentTarget.value)
     }
 
     render () {
         return (
             <div>
-                <ResultatRechercheComponent />
+                <ResultatRechercheComponent onClick={this.handleClick} />
             </div>
         )
     }
